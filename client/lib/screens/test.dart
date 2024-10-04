a94b07909b8eef845783f0775fc7f9a81889580f
ItsZipzon <rune.molander@hotmail.com>
2024-10-04 07:49:57 +0200

    Fixed issue with localhost using android emulator

--- client/lib/screens/test.dart
+++ client/lib/screens/test.dart
@@ -1,11 +1,11 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:client/tools/router_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class Test extends StatefulWidget {
-
   const Test({super.key});
 
   @override
@@ -15,7 +15,6 @@ class Test extends StatefulWidget {
 }
 
 class TestScreenState extends State<Test> {
-
   String message = "";
 
   @override
@@ -25,15 +24,17 @@ class TestScreenState extends State<Test> {
   }
 
   Future<void> fetchMessage() async {
-    bool android = true;
     http.Response response;
 
     try {
-      if (android) {
-        response = await http.get(Uri.parse('http://10.0.2.2:8080/test/map'));
+      String url;
+      if (Platform.isAndroid) {
+        url = 'http://10.0.2.2:8080/test/map';
       } else {
-        response = await http.get(Uri.parse('http://localhost:8080/test/map'));
+        url = 'http://localhost:8080/test/map';
       }
+
+      response = await http.get(Uri.parse(url));
 
       if (response.statusCode == 200) {
         var r = jsonDecode(response.body);
@@ -51,12 +52,11 @@ class TestScreenState extends State<Test> {
       });
     }
   }
-  
+
   @override
   Widget build(BuildContext context) {
-      
     final router = RouterProvider.of(context);
-  
+
     return Scaffold(
       body: Center(
         child: Column(
