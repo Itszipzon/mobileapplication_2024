d551d39329a0083f4bc976462ac5c30dc0bce1ca
ItsZipzon <rune.molander@hotmail.com>
2024-10-05 07:36:21 +0200

    Added some more api requests

--- client/lib/tools/api_handler.dart
+++ client/lib/tools/api_handler.dart
@@ -4,8 +4,54 @@ import 'package:http/http.dart' as http;
 
 class ApiHandler {
 
+  /// Checks if the user is in session.
   static Future<bool> userInSession(String token) async {
-    final response = await http.get(Uri.parse('http://localhost:8080/api/user/insession'));
+    final response = await http.get(Uri.parse('http://localhost:8080/api/user/insession'), headers: {
+      "Authorization" : "Bearer $token"
+    });
+    return jsonDecode(response.body);
+  }
+
+  /// Gets the user.
+  static Future<Map<String,Object>> getUser(String token) async {
+    final response = await http.get(Uri.parse('http://localhost:8080/api/user'), headers: {
+      "Authorization" : "Bearer $token"
+    });
+    return jsonDecode(response.body);
+  }
+
+  /// Logs in the user.
+  static Future<String> login(String email, String password) async {
+    final response = await http.post(Uri.parse('http://localhost:8080/api/user/login'), body: {
+      'email': email,
+      'password': password
+    });
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      return "";
+    }
+  }
+
+  /// Registers the user.
+  static Future<bool> register(String email, String password, String confirmPassword, String username) async {
+    final response = await http.post(Uri.parse('http://localhost:8080/api/user/register'), body: {
+      'email': email,
+      'password': password,
+      'confirmPassword': confirmPassword,
+      'username': username
+    });
+    return jsonDecode(response.body);
+  }
+
+  /// Logs out the user.
+  static Future<bool> logout() async {
+    final response = await http.post(Uri.parse('http://localhost:8080/api/user/logout'));
+    return jsonDecode(response.body);
+  }
+
+  static Future<bool> usernameExists(String username) async {
+    final response = await http.get(Uri.parse('http://localhost:8080/api/user/usernameexists/$username'));
     return jsonDecode(response.body);
   }
 }
