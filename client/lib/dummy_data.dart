1cd7c709f8e571e0b1ebd3c436620647bef3d4b7
runemola <runemola@stud.ntnu.no>
2024-10-14 20:52:21 +0200

    Found dummy image supported by chrome

--- client/lib/dummy_data.dart
+++ client/lib/dummy_data.dart
@@ -2,6 +2,6 @@ class DummyData {
 
   static String thumbnail = 'https://cdn1.epicgames.com/offer/e860fa919120430ca12c557bb676bc6a/EGST_StoreLandscape_2560x1440_2560x1440-e7beda03979167bed00fb2c73bb7998a';
 
-  static String profilePicture = 'https://styles.redditmedia.com/t5_2qijt/styles/communityIcon_tn7yyyq38ei71.png';
+  static String profilePicture = 'https://i1.sndcdn.com/artworks-000262504229-3titw3-t500x500.jpg';
 
 }
