39837a9515705ff1b6efbd1f4fd7df78087f4a80
runemola <runemola@stud.ntnu.no>
2024-10-14 13:08:14 +0200

    Added thumbnail and profilepicture variables

--- client/lib/elements/quiz_post.dart
+++ client/lib/elements/quiz_post.dart
@@ -2,9 +2,13 @@ import 'package:client/tools/router_provider.dart';
 import 'package:flutter/material.dart';
 
 class QuizPost extends StatelessWidget {
-  const QuizPost({super.key, required this.path});
+  const QuizPost({super.key, required this.path,
+  this.thumbnail = "https://cdn1.epicgames.com/offer/e860fa919120430ca12c557bb676bc6a/EGST_StoreLandscape_2560x1440_2560x1440-e7beda03979167bed00fb2c73bb7998a",
+  this.profilePicture = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia-cldnry.s-nbcnews.com%2Fimage%2Fupload%2Fnewscms%2F2021_51%2F1824063%2Fjaden-smith-kb-main-211224.jpg&f=1&nofb=1&ipt=d85d8ff12568b5a86082486d5c62f44e2462dd0691d08949907688fecbfbd696&ipo=images"});
 
   final String path;
+  final String thumbnail;
+  final String profilePicture;
 
   @override
   Widget build(BuildContext context) {
@@ -16,34 +20,30 @@ class QuizPost extends StatelessWidget {
         child: SizedBox(
           child: Container(
               padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: const Column(
+              child: Column(
                 mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Image(
-                    image: NetworkImage(
-                      "https://cdn1.epicgames.com/offer/e860fa919120430ca12c557bb676bc6a/EGST_StoreLandscape_2560x1440_2560x1440-e7beda03979167bed00fb2c73bb7998a",
-                    ),
+                    image: NetworkImage(thumbnail),
                     height: 96,
                     width: 212,
                     fit: BoxFit.cover,
                   ),
-                  SizedBox(height: 8),
+                  const SizedBox(height: 8),
                   Row(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       ClipOval(
                         child: Image(
-                          image: NetworkImage(
-                            "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia-cldnry.s-nbcnews.com%2Fimage%2Fupload%2Fnewscms%2F2021_51%2F1824063%2Fjaden-smith-kb-main-211224.jpg&f=1&nofb=1&ipt=d85d8ff12568b5a86082486d5c62f44e2462dd0691d08949907688fecbfbd696&ipo=images",
-                          ),
+                          image: NetworkImage(profilePicture),
                           height: 50,
                           width: 50,
                           fit: BoxFit.cover,
                         ),
                       ),
-                      SizedBox(width: 8),
-                      Column(
+                      const SizedBox(width: 8),
+                      const Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Text("Starwars Quiz"),
