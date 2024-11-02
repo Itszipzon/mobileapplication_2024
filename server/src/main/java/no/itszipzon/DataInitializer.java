858afcc81105238118cf4191bc522b40cd5af0ac
ItsZipzon <rune.molander@hotmail.com>
2024-11-02 07:59:56 +0100

    Added initial categories

--- server/src/main/java/no/itszipzon/DataInitializer.java
+++ server/src/main/java/no/itszipzon/DataInitializer.java
@@ -1,5 +1,9 @@
 package no.itszipzon;
 
+import java.util.List;
+import no.itszipzon.repo.CategoryRepo;
+import no.itszipzon.tables.Category;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -9,9 +13,52 @@ import org.springframework.stereotype.Component;
 @Component
 public class DataInitializer implements CommandLineRunner {
 
+  @Autowired
+  private CategoryRepo categoryRepo;
+
   @Override
   public void run(String... args) throws Exception {
     // Add code to run when the application starts
+    createCategories();
+  }
+
+  private void createCategories() {
+    List<Category> existing = categoryRepo.findAll();
+    List<String> categories = List.of(
+        "General Knowledge",
+        "Science",
+        "History",
+        "Geography",
+        "Art",
+        "Sports",
+        "Music",
+        "Movies",
+        "Literature",
+        "Technology",
+        "Nature",
+        "Food & Drink",
+        "Animals",
+        "Mythology",
+        "Politics",
+        "Celebrities",
+        "Vehicles",
+        "Comics",
+        "Anime",
+        "Cartoons",
+        "Video Games",
+        "Board Games",
+        "Fashion",
+        "Gardening",
+        "Health"
+        );
+    
+    for (String category : categories) {
+      if (existing.stream().noneMatch(c -> c.getName().equals(category))) {
+        Category newCategory = new Category();
+        newCategory.setName(category);
+        categoryRepo.save(newCategory);
+      }
+    }
   }
 
 }
