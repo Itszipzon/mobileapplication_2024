cf6b54740eefb1210cf525b44f6a169ef32ad5e1
ItsZipzon <rune.molander@hotmail.com>
2024-11-02 14:23:01 +0100

    Some changes

--- client/lib/elements/quiz_post.dart
+++ client/lib/elements/quiz_post.dart
@@ -25,16 +25,34 @@ class QuizPost extends ConsumerWidget {
     final difference = now.difference(date);
 
     if (difference.inDays >= 365) {
+      if ((difference.inDays / 365).floor() == 1) {
+        return '1 year ago';
+      }
       return '${(difference.inDays / 365).floor()} years ago';
     } else if (difference.inDays >= 30) {
+      if ((difference.inDays / 30).floor() == 1) {
+        return '1 month ago';
+      }
       return '${(difference.inDays / 30).floor()} months ago';
     } else if (difference.inDays >= 7) {
+      if ((difference.inDays / 7).floor() == 1) {
+        return '1 week ago';
+      }
       return '${(difference.inDays / 7).floor()} weeks ago';
     } else if (difference.inDays > 0) {
+      if (difference.inDays == 1) {
+        return '1 day ago';
+      }
       return '${difference.inDays} days ago';
     } else if (difference.inHours > 0) {
+      if (difference.inHours == 1) {
+        return '1 hour ago';
+      }
       return '${difference.inHours} hours ago';
     } else if (difference.inMinutes > 0) {
+      if (difference.inMinutes == 1) {
+        return '1 minute ago';
+      }
       return '${difference.inMinutes} minutes ago';
     } else {
       return 'Just now';
