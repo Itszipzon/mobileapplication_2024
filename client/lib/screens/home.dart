0d190528961ba13987b232485880247066119610
ItsZipzon <rune.molander@hotmail.com>
2024-11-15 23:13:06 +0100

    Optimized screen

--- client/lib/screens/home.dart
+++ client/lib/screens/home.dart
@@ -17,8 +17,9 @@ class Home extends ConsumerStatefulWidget {
 class HomeState extends ConsumerState<Home> {
   late final RouterNotifier router;
   late final UserNotifier user;
-  late Future<List<Map<String, dynamic>>> _quizDataFuture;
-  late Future<List<Map<String, dynamic>>> _popularQuizDataFuture;
+  List<Map<String, dynamic>>? _recentQuizData;
+  List<Map<String, dynamic>>? _popularQuizData;
+  bool loading = true;
 
   @override
   void initState() {
@@ -26,67 +27,52 @@ class HomeState extends ConsumerState<Home> {
     WidgetsBinding.instance.addPostFrameCallback((_) {
       router = ref.read(routerProvider.notifier);
       user = ref.read(userProvider.notifier);
+
+      _initQuizzes();
     });
-    _quizDataFuture = ApiHandler.getQuizzesByFilter(0, 5, "createdAt", "DESC");
-    _popularQuizDataFuture = ApiHandler.getMostPopularQuizzes(0);
+  }
+
+  Future<void> _initQuizzes() async {
+    ApiHandler.getQuizzesByFilter(0, 5, "createdAt", "DESC").then((value) => {
+          setState(() {
+            _recentQuizData = value;
+          })
+        });
+    ApiHandler.getMostPopularQuizzes(0).then((value) => {
+          setState(() {
+            _popularQuizData = value;
+          })
+        });
+
+    setState(() {
+      loading = false;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 241, 241, 241),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            // Section for Recently Created Quizzes
-            FutureBuilder<List<Map<String, dynamic>>>(
-              future: _quizDataFuture,
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(child: LogoLoading());
-                } else if (snapshot.hasError) {
-                  return Center(
-                      child: Text(
-                          'Error loading recent quizzes: ${snapshot.error}'));
-                } else if (snapshot.hasData) {
-                  final quizData = snapshot.data!;
-                  return Container(
-                    padding: const EdgeInsets.all(8),
-                    child: FeedCategory(
-                        category: "Recently Created", quizzes: quizData),
-                  );
-                } else {
-                  return const Center(
-                      child: Text('No recent quizzes available.'));
-                }
-              },
+      body: loading
+          ? const Center(
+              child: LogoLoading(),
+            )
+          : Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  child: FeedCategory(
+                      category: "Recently Created",
+                      quizzes: _recentQuizData ?? []),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  child: FeedCategory(
+                      category: "Popular Quizzes",
+                      quizzes: _popularQuizData ?? []),
+                ),
+              ],
             ),
-            // Section for Popular Quizzes
-            FutureBuilder<List<Map<String, dynamic>>>(
-              future: _popularQuizDataFuture,
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(child: LogoLoading());
-                } else if (snapshot.hasError) {
-                  return Center(
-                      child: Text(
-                          'Error loading popular quizzes: ${snapshot.error}'));
-                } else if (snapshot.hasData) {
-                  final popularQuizData = snapshot.data!;
-                  return Container(
-                    padding: const EdgeInsets.all(8),
-                    child: FeedCategory(
-                        category: "Popular Quizzes", quizzes: popularQuizData),
-                  );
-                } else {
-                  return const Center(
-                      child: Text('No popular quizzes available.'));
-                }
-              },
-            ),
-          ],
-        ),
-      ),
       bottomNavigationBar: const BottomNavbar(
         path: "home",
       ),
