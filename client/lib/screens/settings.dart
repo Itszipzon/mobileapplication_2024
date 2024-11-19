4eebac9ee15890356aaacdd1da9eeb39dedaa4e7
Phillip M Overå <phillipo@stud.ntnu.no>
2024-11-19 17:51:03 +0100

    Started on settings page

---
+++ client/lib/screens/settings.dart
@@ -0,0 +1,36 @@
+import 'package:client/tools/router.dart';
+import 'package:client/tools/user.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class Settings extends ConsumerStatefulWidget {
+
+  const Settings({super.key});
+
+  @override
+  SettingsState createState() => SettingsState();
+
+}
+
+class SettingsState extends ConsumerState<Settings> {
+  late final RouterNotifier router;
+  late final UserNotifier user;
+  bool loading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      router = ref.read(routerProvider.notifier);
+      user = ref.read(userProvider.notifier);
+      loading = false;
+    });
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+
+}
