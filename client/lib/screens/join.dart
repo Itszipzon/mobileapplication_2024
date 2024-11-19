d5d2c3cd0690ae91dbd4c1b66c1b40cf872a6a43
runemola <runemola@stud.ntnu.no>
2024-11-19 18:41:48 +0100

    Changed from token to game pin

--- client/lib/screens/join.dart
+++ client/lib/screens/join.dart
@@ -47,7 +47,7 @@ class JoinState extends ConsumerState<Join> {
             children: [
               Expanded(
                 child: Input(
-                    labelText: "Game Pin", controller: codeController, onReturn: (_) => onPressed(),),
+                    labelText: "Game PIN", controller: codeController, onReturn: (_) => onPressed(),),
               ),
               const SizedBox(width: 5),
               SizedTextButton(
