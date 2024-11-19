26fec6e80b151dad9d62a960a71d6ffb963f421e
ItsZipzon <rune.molander@hotmail.com>
2024-11-19 23:15:10 +0100

    Checkstyle

--- server/src/main/java/no/itszipzon/tables/QuizOption.java
+++ server/src/main/java/no/itszipzon/tables/QuizOption.java
@@ -1,10 +1,6 @@
 package no.itszipzon.tables;
 
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
-
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
