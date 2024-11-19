67ddffbb60d560a91c0d5220d4d2517126776e17
runemola <runemola@stud.ntnu.no>
2024-11-19 18:19:33 +0100

    Top of profile on settings

--- client/lib/screens/settings.dart
+++ client/lib/screens/settings.dart
@@ -1,4 +1,7 @@
+import 'package:client/dummy_data.dart';
 import 'package:client/elements/bottom_navbar.dart';
+import 'package:client/elements/button.dart';
+import 'package:client/elements/profile_picture.dart';
 import 'package:client/tools/router.dart';
 import 'package:client/tools/user.dart';
 import 'package:flutter/material.dart';
@@ -18,6 +21,10 @@ class SettingsState extends ConsumerState<Settings> {
   late final RouterNotifier router;
   late final UserNotifier user;
   bool loading = true;
+  Map<String, dynamic> profile = {
+    "username": "",
+    "pfp": DummyData.profilePicture,
+  };
 
   @override
   void initState() {
@@ -26,15 +33,90 @@ class SettingsState extends ConsumerState<Settings> {
       router = ref.read(routerProvider.notifier);
       user = ref.read(userProvider.notifier);
       loading = false;
+      _getProfile();
     });
-
   }
+
+    Future<void> _getProfile() async {
+    user.getProfile().then((value) {
+      setState(() {
+        profile = value;
+      });
+    });
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: Center(
-          child: Text("Settings"),
+        margin: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            // Profile header section
+            Row(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    print("Not implemented yet");
+                  },
+                  child: ClipOval(
+                    child: ProfilePicture(url: profile["pfp"]),
+                  ),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  "${profile["username"]}",
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )
+              ],
+            ),
+            const SizedBox(height: 10),
+
+            // Button row
+            Container(
+              width: MediaQuery.of(context).size.width,
+              padding: const EdgeInsets.symmetric(horizontal: 4),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: SizedTextButton(
+                      text: "Settings",
+                      onPressed: () => {router.setPath(context, "settings")},
+                      height: 40,
+                      textStyle:
+                          const TextStyle(fontSize: 16, color: Colors.white),
+                    ),
+                  ),
+                  const SizedBox(width: 4),
+                  Expanded(
+                    child: SizedTextButton(
+                      text: "Friends",
+                      onPressed: () => router.setPath(context, "friends"),
+                      height: 40,
+                      textStyle:
+                          const TextStyle(fontSize: 16, color: Colors.white),
+                    ),
+                  ),
+                  const SizedBox(width: 4),
+                  Expanded(
+                    child: SizedTextButton(
+                      text: "Sign out",
+                      onPressed: () => {user.logout(context, router)},
+                      height: 40,
+                      textStyle:
+                          const TextStyle(fontSize: 16, color: Colors.white),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            ],
         ),
       ),
       bottomNavigationBar: BottomNavbar(path: "profile"),
