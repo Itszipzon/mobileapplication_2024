3c296f8c6f6acd068b54f21c05ec60fd0123d86e
ItsZipzon <rune.molander@hotmail.com>
2024-11-26 05:26:26 +0100

    Made it create VPC if not existing, if it does exist use existing

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ locals {
 # Create a subnet
 resource "aws_subnet" "main" {
   vpc_id                  = local.vpc_id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = "10.0.2.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "eu-west-2a"
   tags = {
