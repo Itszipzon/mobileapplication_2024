c3b3e338651f7ac9ca77adab317ea42aef243161
avnit-1302 <122270703+avnit-1302@users.noreply.github.com>
2024-11-27 22:51:50 +0100

    added query method for deleting

--- server/src/main/java/no/itszipzon/repo/ResetTokenRepo.java
+++ server/src/main/java/no/itszipzon/repo/ResetTokenRepo.java
@@ -1,13 +1,27 @@
 package no.itszipzon.repo;
 
 import no.itszipzon.tables.ResetToken;
+import no.itszipzon.tables.User;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-
+@Repository
 public interface ResetTokenRepo extends JpaRepository<ResetToken, Long> {
+
     Optional<ResetToken> findByToken(String token);
 
-    void deleteByUser_Email(String email);
+    void deleteByUser_UserId(Long userId);
+
+    Optional<ResetToken> findByUser_UserIdAndValidTrue(Long userId);
+
+    @Modifying
+    @Query("DELETE FROM ResetToken rt WHERE rt.user = :user")
+    void deleteByUser(@Param("user") User user);
+
 }
