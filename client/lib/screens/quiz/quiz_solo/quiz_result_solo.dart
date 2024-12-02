fcf5d76d4b89dc88ed0fd8fecd40b2a4daa5ea27
Jan Nordskog <jan.nordskog@gmail.com>
2024-12-02 17:59:46 +0100

    fixed z index of leave button in quiz results

--- client/lib/screens/quiz/quiz_solo/quiz_result_solo.dart
+++ client/lib/screens/quiz/quiz_solo/quiz_result_solo.dart
@@ -70,97 +70,125 @@ class QuizResultSoloState extends ConsumerState<QuizResultSolo> {
   @override
   Widget build(BuildContext context) {
     return loading
-        ? Center(
-            child: LogoLoading(),
-          )
-        : Container(
-            margin: const EdgeInsets.all(16),
-            child: Column(
+        ? Center(child: LogoLoading())
+        : Scaffold(
+            body: Column(
               children: [
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Container(
+                      margin: const EdgeInsets.all(16),
+                      child: Column(
+                        children: [
+                          Container(
+                            padding: const EdgeInsets.all(16),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(8),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey.withOpacity(0.3),
+                                  spreadRadius: 3,
+                                  blurRadius: 5,
+                                  offset: const Offset(0, 3),
+                                ),
+                              ],
+                            ),
+                            child: Text(
+                              "${performanceComment()} Scoring ${quizScore["score"]} out of ${widget.totalQuestions * 1000} shows your knowledge of ${Tools.fixEncoding(widget.quizData["title"] ?? "Unknown Title")}.",
+                              textAlign: TextAlign.center,
+                              style: const TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 16),
+                          Column(
+                            children: quizScore["checks"].map<Widget>((check) {
+                              final questionId = check["questionId"];
+                              final answerCorrect = check["correct"] ?? false;
+                              final questionData = widget
+                                  .quizData["quizQuestions"]
+                                  ?.firstWhere((q) => q["id"] == questionId,
+                                      orElse: () => {});
+
+                              final questionText = Tools.fixEncoding(
+                                  questionData?["question"] ?? "No Question");
+
+                              return Container(
+                                margin: const EdgeInsets.symmetric(vertical: 8),
+                                padding: const EdgeInsets.all(12),
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(8),
+                                  border: Border.all(
+                                    color: answerCorrect
+                                        ? Colors.green
+                                        : Colors.red,
+                                    width: 2,
+                                  ),
+                                ),
+                                child: Row(
+                                  children: [
+                                    Expanded(
+                                      child: Text(
+                                        "${quizScore["checks"].indexOf(check) + 1}. $questionText",
+                                        style: const TextStyle(fontSize: 16),
+                                      ),
+                                    ),
+                                    Icon(
+                                      answerCorrect
+                                          ? Icons.check_circle
+                                          : Icons.cancel,
+                                      color: answerCorrect
+                                          ? Colors.green
+                                          : Colors.red,
+                                      size: 28,
+                                    ),
+                                  ],
+                                ),
+                              );
+                            }).toList(),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
                 Container(
                   padding: const EdgeInsets.all(16),
                   decoration: BoxDecoration(
                     color: Colors.white,
-                    borderRadius: BorderRadius.circular(8),
                     boxShadow: [
                       BoxShadow(
                         color: Colors.grey.withOpacity(0.3),
-                        spreadRadius: 3,
-                        blurRadius: 5,
-                        offset: const Offset(0, 3),
+                        spreadRadius: 1,
+                        blurRadius: 3,
+                        offset: const Offset(0, -2),
                       ),
                     ],
                   ),
-                  child: Text(
-                    "${performanceComment()} Scoring ${quizScore["score"]} out of ${widget.totalQuestions * 1000} shows your knowledge of ${Tools.fixEncoding(widget.quizData["title"] ?? "Unknown Title")}.",
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Theme.of(context).primaryColor,
+                      padding: const EdgeInsets.symmetric(vertical: 16),
+                      minimumSize: const Size.fromHeight(50),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.zero,
+                      ),
                     ),
-                  ),
-                ),
-                const SizedBox(height: 16),
-                Column(
-                  children: quizScore["checks"].map<Widget>((check) {
-                    final questionId = check["questionId"];
-                    final answerCorrect = check["correct"] ?? false;
-                    final questionData = widget.quizData["quizQuestions"]
-                        ?.firstWhere((q) => q["id"] == questionId,
-                            orElse: () => {});
-
-                    final questionText =
-                        Tools.fixEncoding(questionData?["question"] ?? "No Question");
-
-                    return Container(
-                      margin: const EdgeInsets.symmetric(vertical: 8),
-                      padding: const EdgeInsets.all(12),
-                      decoration: BoxDecoration(
+                    onPressed: () {
+                      final router = ref.read(routerProvider.notifier);
+                      router.setPath(context, "home");
+                    },
+                    child: const Text(
+                      "Leave Quiz",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
                         color: Colors.white,
-                        borderRadius: BorderRadius.circular(8),
-                        border: Border.all(
-                          color: answerCorrect ? Colors.green : Colors.red,
-                          width: 2,
-                        ),
                       ),
-                      child: Row(
-                        children: [
-                          Expanded(
-                            child: Text(
-                              "${quizScore["checks"].indexOf(check) + 1}. $questionText",
-                              style: const TextStyle(fontSize: 16),
-                            ),
-                          ),
-                          Icon(
-                            answerCorrect ? Icons.check_circle : Icons.cancel,
-                            color: answerCorrect ? Colors.green : Colors.red,
-                            size: 28,
-                          ),
-                        ],
-                      ),
-                    );
-                  }).toList(),
-                ),
-                const SizedBox(height: 16),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Theme.of(context).primaryColor,
-                    padding: const EdgeInsets.symmetric(vertical: 16),
-                    minimumSize: const Size.fromHeight(50),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.zero,
-                    ),
-                  ),
-                  onPressed: () {
-                    final router = ref.read(routerProvider.notifier);
-                    router.setPath(context, "home");
-                  },
-                  child: const Text(
-                    "Leave Quiz",
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
                     ),
                   ),
                 ),
